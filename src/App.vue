3fb51a80a037303b86b6b7bebcb806138dc98812
Siddhesh Mangela <sidthecool007@gmail.com>
2021-02-15 11:21:34 +0530

    init

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <header></header>
+  <main></main>
+  <nav></nav>
+</template>
+
+<script setup></script>
+
+<style lang="scss">
+body {
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  min-height: 100vh;
+  font-family: 'Rubik', sans-serif;
+  margin: 0;
+  padding: 0;
+}
+
+html {
+  margin: 0;
+  padding: 0;
+}
+
+* {
+  box-sizing: border-box;
+}
+
+#app {
+  min-height: inherit;
+}
+</style>
