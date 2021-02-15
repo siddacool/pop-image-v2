82ea3b4f6691155e877900b2dc54173710eb1318
Siddhesh Mangela <sidthecool007@gmail.com>
2021-02-15 15:13:18 +0530

    Light dark button added

--- src/components/ThemeToggleButton.vue
+++ src/components/ThemeToggleButton.vue
@@ -1,10 +1,21 @@
 <template>
-  <button @click="toggleTheme" class="btn theme-toggle-button">Dark</button>
+  <button @click="handleThemeToggle" class="btn theme-toggle-button">
+    {{ isDarkTheme ? 'Light' : 'Dark' }}
+  </button>
 </template>
 
 <script setup>
 /* eslint-disable no-unused-vars */
+import { ref } from 'vue';
+import { getStorage, updateStorage } from '../storage';
+const { darkTheme } = getStorage();
 import { toggleTheme } from '../utils';
+const isDarkTheme = ref(getStorage().darkTheme);
+
+const handleThemeToggle = () => {
+  toggleTheme();
+  isDarkTheme.value = !isDarkTheme.value;
+};
 </script>
 
 <style lang="scss" scoped></style>
